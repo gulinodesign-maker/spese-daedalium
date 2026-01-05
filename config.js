5d4aafe33eab375c6c8510f8f062745aea23c283
gulinodesign-maker <gulinodesign@gmail.com>
2026-01-05 08:58:00 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-// URL della tua Web App Apps Script
-const API_BASE_URL =
-  "https://script.google.com/macros/s/AKfycbxw-OfBbRDWtWl4ETSnXllHg0i17wyNWON8oyjerPzzR_qz6EJ5eDRw0u3pTZnJa_4/exec";
-
-// Deve combaciare con API_KEY in Code.gs (scegli una chiave e mettila uguale anche in apps-script/Code.gs)
+/* dDAE Config - dDAE_1.036 */
+const API_BASE_URL = "https://script.google.com/macros/s/AKfycbxw-OfBbRDWtWl4ETSnXllHg0i17wyNWON8oyjerPzzR_qz6EJ5eDRw0u3pTZnJa_4/exec";
 const API_KEY = "daedalium2026";
