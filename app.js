0037e718836cecae79ad03c3c5d82caf0a0a2dec
gulinodesign-maker <gulinodesign@gmail.com>
2026-01-10 01:07:35 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 /**
  * Build: incrementa questa stringa alla prossima modifica (es. 1.001)
  */
-const BUILD_VERSION = "1.124";
+const BUILD_VERSION = "1.125";
 
 
 // ===== Stato UI: evita "torna in HOME" quando iOS aggiorna il Service Worker =====
